ef47357bd31c8a76888c645ba011d045411125a2
jonw-cogapp <jonw@cogapp.com>
2020-07-31 13:02:23 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = (config) => {
+  
+  config.addPassthroughCopy('admin/config.yml');
+  config.addPassthroughCopy('admin/index.html');
+
+  config.setTemplateFormats([
+    "css", // css is not yet a recognized template extension in Eleventy
+    "md",
+    "njk"
+  ]);
+
+  return {
+    dir: {
+      output: "_site",
+      input: "content",
+      includes: "templates",
+    }
+  }
+}
---
+++ content/test.md
@@ -0,0 +1,5 @@
+---
+title: "Hello"
+description: "Test"
+permalink: "index.html"
+---
