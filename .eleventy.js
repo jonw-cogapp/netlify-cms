16ca2f752c30c7b6a630ceefe5f1eb8ca5642d5e
jonw-cogapp <jonw@cogapp.com>
2020-08-04 12:29:56 +0100

    finishing up

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = (config) => {
   
   config.addPassthroughCopy('admin/config.yml');
   config.addPassthroughCopy('admin/index.html');
+  config.addPassthroughCopy('assets');
+  config.addPassthroughCopy({'static/images/uploads': 'images/uploads'});
 
   config.setTemplateFormats([
     "css", // css is not yet a recognized template extension in Eleventy
